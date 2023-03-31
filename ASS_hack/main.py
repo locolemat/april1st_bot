3511f7b64a2156c6b360f19d1fa8d7f89dec78dd
ivkrak <iv.krak@gmail.com>
2023-04-01 01:10:35 +0800

    Саня гей

--- ASS_hack/main.py
+++ ASS_hack/main.py
@@ -1,1 +1,28 @@
-# Саня гей
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+API_TOKEN = '172233586:AAF1EdyToa0DpWc3JNgDvfDQGXnrXKkFVxw'
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help', 'restart'])
+async def welcome(message: types.Message):
+    rq = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+    rq.add(KeyboardButton('Запуск видео'))
+    rq.add(KeyboardButton('Выдать скриммер'))
+    await message.reply(
+        'Привет, устроим взлом ЖЕПЫ?)'
+    )
+    await bot.send_sticker(chat_id=message.chat.id,
+                           sticker='CAACAgIAAxkBAAEIZwNkJxJjU8kQVV4oulNFb2Ddwiy51wAC1AoAAuhO8Uh29Jn8lOf2ii8E',
+                           reply_markup=rq
+                           )
+
+@dp.message_handler()
+async def asshack(message: types.message):
+    match message.text:
+        case 'Запуск видео':
+            pass
+        case 'Выдать скриммер':
+            pass
